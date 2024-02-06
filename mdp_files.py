685a9ac2baadd3734ee4b6024b1b4d78d82a27ad
LindeSchoenmaker <l.schoenmaker@lacdr.leidenuniv.nl>
2024-02-06 21:50:55 +0100

    write more frames during production for first and final lambda

--- mdp_files.py
+++ mdp_files.py
@@ -11,20 +11,17 @@ if __name__ == "__main__":
         with open(template, 'r') as f:
             content = f.read()
 
-        if softcore:
-            for i in range(11):
-                with open(f'input/mdppath/files/{protocol}.{i}.mdp', 'w') as f:
-                    content_new = content.replace('XXX', str(i))
-                    f.write(content_new)
-
-        else:
-            for i in range(19):
-                with open(f'input/mdppath/files/{protocol}.{i}.mdp', 'w') as f:
-                    if i <= 4:
-                        content_new = content.replace('XXX', 'A')
-                    if i >= 5 and i <= 14:
-                        content_new = content.replace('XXX', 'AB')
-                    elif i >= 15:
-                        content_new = content.replace('XXX', 'B')
-                    content_new = content_new.replace('YYY', str(i))
-                    f.write(content_new)
+        for i in range(19):
+            with open(f'input/mdppath/files/{protocol}.{i}.mdp', 'w') as f:
+                if i <= 4:
+                    content_new = content.replace('XXX', 'A')
+                if i >= 5 and i <= 14:
+                    content_new = content.replace('XXX', 'AB')
+                elif i >= 15:
+                    content_new = content.replace('XXX', 'B')
+                if i ==0 or i==18:
+                    content_new = content_new.replace('ZZZ', '500')
+                else:
+                    content_new = content_new.replace('ZZZ', '5000')
+                content_new = content_new.replace('YYY', str(i))
+                f.write(content_new)
