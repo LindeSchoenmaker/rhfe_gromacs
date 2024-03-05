96b564e142beca5acfd552ad52a9d674188c27c7
LindeSchoenmaker <l.schoenmaker@lacdr.leidenuniv.nl>
2024-03-05 15:08:58 +0100

    also merge polar hydrogens

--- AZtutorial.py
+++ AZtutorial.py
@@ -299,12 +299,14 @@ class AZtutorial:
                                 '-i2',i2,
                                 '-o1',o1,
                                 '-o2',o2,
+                                '-'
                                 '-opdb1',opdb1,
                                 '-opdb2',opdb2,                                        
                                 '-opdbm1',opdbm1,
                                 '-opdbm2',opdbm2,
                                 '-score',score,
-                                '-log',log],
+                                '-log',log,
+                                '-H2Hpolar',True],
                                 stdout=subprocess.PIPE, 
                                 stderr=subprocess.PIPE)
 
